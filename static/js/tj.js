778bdb919b8a1ef7af4c986300bbfb955ad2cf30
lrkkevin <lrkkevin@163.com>
2020-09-10 13:12:49 +0800

    增加vfed模版和修改默认配置和配置文件.

--- static/js/tj.js
+++ static/js/tj.js
@@ -1,1 +1,15 @@
-document.write('统计代码')
+document.write('<!-- Default Statcounter code for 52xiuxiutv http://52xiuxiutv.com/ -->
+<script type="text/javascript">
+var sc_project=12380448; 
+var sc_invisible=1; 
+var sc_security="0fcd9857"; 
+var sc_https=1; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js" async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/12380448/0/0fcd9857/1/" alt="Web
+Analytics"></a></div></noscript>
+<!-- End of Statcounter Code -->
+<a href="https://statcounter.com/p12380448/?guest=1">View My Stats</a>')
